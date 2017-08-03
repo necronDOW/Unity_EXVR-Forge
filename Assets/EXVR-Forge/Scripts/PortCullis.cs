b2477e855685c8de240af30d5018e6e78d9997d7
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-08-03 14:14:47 +0100

    Need to research inter-scene networking outside of uni.

---
+++ Assets/EXVR-Forge/Scripts/PortCullis.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class PortCullis : MonoBehaviour
+{
+    public string targetSceneName;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            NetworkManager.singleton.ServerChangeScene(targetSceneName);
+    }
+}
