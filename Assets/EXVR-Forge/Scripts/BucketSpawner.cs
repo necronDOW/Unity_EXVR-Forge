4c520b363d4c16e98b5ab7cbd3b0d774f9484ef5
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-08-04 11:57:11 +0100

    Ported old scripts across and more enviroment

---
+++ Assets/EXVR-Forge/Scripts/BucketSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Transform))]
+
+public class BucketSpawner : MonoBehaviour
+{
+    public Object prefab;
+    public float spawnRate = 1;
+
+    void Update()
+    {
+        if ((transform.up.y * 2f) < (transform.position.y - 1f))
+            SpawnPrefab();
+        else ticks = 0;
+    }
+
+    private float ticks = 0;
+    private void SpawnPrefab()
+    {
+        ticks += Time.deltaTime;
+
+        if (ticks >= spawnRate)
+        {
+            float rand = Random.Range(-0.1f, 0.1f);
+            Vector3 offset = new Vector3(rand, rand, rand);
+
+            GameObject instance = (GameObject)Instantiate(prefab, transform.position + offset, transform.rotation);
+            instance.GetComponent<Rigidbody>().AddForce(instance.transform.up * 100f, ForceMode.Force);
+            ticks = 0;
+        }
+    }
+}
---
+++ Assets/EXVR-Forge/Scripts/ProSkaterScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProSkaterScript : MonoBehaviour
+{
+    public float variance;
+    public float floatSpeed;
+    public Vector3 rotation;
+    public Color activeEmission;
+    public float inactiveRamp = 0.5f;
+
+    private float t = 0.5f;
+    private Vector3 lowerBound;
+    private Vector3 upperBound;
+    private Color defaultColor;
+    private Color defaultEmission;
+    private MeshRenderer mR;
+    private SphereCollider sC;
+
+    void Start()
+    {
+        lowerBound = transform.position - new Vector3(0, variance, 0);
+        upperBound = transform.position + new Vector3(0, variance, 0);
+
+        mR = GetComponent<MeshRenderer>();
+        mR.material.EnableKeyword("_EMISSION");
+        defaultColor = mR.material.color;
+        defaultEmission = mR.material.GetColor("_EmissionColor");
+        sC = GetComponent<SphereCollider>();
+
+        SetActive(false);
+    }
+    
+    void Update()
+    {
+        t += floatSpeed;
+
+        transform.position = new Vector3(transform.position.x, 
+            Mathf.Lerp(lowerBound.y, upperBound.y, t), transform.position.z);
+
+        if (t >= 1f || t <= 0)
+            floatSpeed *= -1;
+
+        transform.Rotate(rotation);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.tag == "VrController")
+            mR.material.SetColor("_EmissionColor", activeEmission);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.attachedRigidbody.tag == "VrController")
+            mR.material.SetColor("_EmissionColor", defaultEmission);
+    }
+
+    public void SetActive(bool value)
+    {
+        sC.enabled = value;
+
+        if (value)
+        {
+            mR.material.color = defaultColor;
+            mR.material.SetColor("_EmissionColor", defaultEmission);
+        }
+        else
+        {
+            mR.material.color = new Color(defaultColor.r, defaultColor.g, defaultColor.b, defaultColor.a) * inactiveRamp;
+            mR.material.SetColor("_EmissionColor", new Color(defaultEmission.r, defaultEmission.g, defaultEmission.b) * inactiveRamp);
+        }
+    }
+}
