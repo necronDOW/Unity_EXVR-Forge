58e98b7eaf9a370eb8bea26ca2bbaa62cb930dfe
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-08-09 13:46:22 +0100

    Added new attach point script for anvil

--- Assets/EXVR-Forge/Scripts/Tools/AttachPoint.cs
+++ Assets/EXVR-Forge/Scripts/Tools/AttachPoint.cs
@@ -1,102 +1,106 @@
 ﻿using UnityEngine;
-using Valve.VR;
+using Valve.VR.InteractionSystem;
 using System.Collections;
 
 [RequireComponent(typeof(MeshRenderer))]
 
+
 public class AttachPoint : MonoBehaviour
 {
-    public string[] toolNames;
+    
 
-    private MeshRenderer mRenderer;
-    private NVRInteractable inCollider;
-    private GameObject attached;
 
-    void Start()
-    {
-        mRenderer = GetComponent<MeshRenderer>();
-    }
 
-    void Update()
-    {
-        if (attached)
-        {
-            attached.transform.position = transform.position;
-            attached.transform.forward = transform.forward;
 
-            if (attached.GetComponent<Interactable>().IsAttached)
-                DetachObject();
-        }
+    //private MeshRenderer mRenderer;
+    //private Interactable inCollider;
+    //private GameObject attached;
 
-        if (!inCollider)
-        {
-            for (int i = 0; i < toolNames.Length; i++)
-            {
-                if (GameObject.Find(toolNames[i]).GetComponent<NVRInteractable>().IsAttached && !mRenderer.enabled)
-                {
-                    mRenderer.enabled = true;
-                    break;
-                }
-                else if (!GameObject.Find(toolNames[i]).GetComponent<NVRInteractable>().IsAttached && mRenderer.enabled)
-                    mRenderer.enabled = false;
-            }
-        }
-        else
-        {
-            if (!inCollider.IsAttached && !attached)
-                AttachObject(inCollider.gameObject);
-        }
-    }
+    //void Start()
+    //{
+    //    mRenderer = GetComponent<MeshRenderer>();
+    //}
 
-    void OnTriggerEnter(Collider other)
-    {
-        for (int i = 0; i < toolNames.Length; i++)
-        {
-            if (other.name == toolNames[i])
-            {
-                inCollider = other.GetComponent<NVRInteractable>();
+    //void Update()
+    //{
+    //    if (attached)
+    //    {
+    //        attached.transform.position = transform.position;
+    //        attached.transform.forward = transform.forward;
 
-                if (other.GetComponent<NVRInteractable>().IsAttached)
-                    mRenderer.enabled = false;
-            }
-        }
-    }
+    //        if (attached.GetComponent<Interactable>().atta)
+    //            DetachObject();
+    //    }
 
-    void OnTriggerExit(Collider other)
-    {
-        for (int i = 0; i < toolNames.Length; i++)
-        {
-            if (other.name == toolNames[i])
-            {
-                inCollider = null;
+    //    if (!inCollider)
+    //    {
+    //        for (int i = 0; i < toolNames.Length; i++)
+    //        {
+    //            if (GameObject.Find(toolNames[i]).GetComponent<NVRInteractable>().IsAttached && !mRenderer.enabled)
+    //            {
+    //                mRenderer.enabled = true;
+    //                break;
+    //            }
+    //            else if (!GameObject.Find(toolNames[i]).GetComponent<NVRInteractable>().IsAttached && mRenderer.enabled)
+    //                mRenderer.enabled = false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (!inCollider.IsAttached && !attached)
+    //            AttachObject(inCollider.gameObject);
+    //    }
+    //}
 
-                if (other.GetComponent<NVRInteractable>().IsAttached)
-                    mRenderer.enabled = true;
-            }
-        }
-    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    for (int i = 0; i < toolNames.Length; i++)
+    //    {
+    //        if (other.name == toolNames[i])
+    //        {
+    //            inCollider = other.GetComponent<Interactable>();
 
-    public void AttachObject(GameObject o)
-    {
-        if (!attached)
-        {
-            attached = o;
-            attached.transform.position = transform.position;
-            attached.transform.up = transform.up;
+    //            if (other.GetComponent<NVRInteractable>().IsAttached)
+    //                mRenderer.enabled = false;
+    //        }
+    //    }
+    //}
 
-            if (attached.GetComponent<ToolScript>())
-                attached.GetComponent<ToolScript>().SetReady(true);
-        }
-    }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    for (int i = 0; i < toolNames.Length; i++)
+    //    {
+    //        if (other.name == toolNames[i])
+    //        {
+    //            inCollider = null;
 
-    public void DetachObject()
-    {
-        if (attached)
-        {
-            if (attached.GetComponent<ToolScript>())
-                attached.GetComponent<ToolScript>().SetReady(false);
+    //            if (other.GetComponent<NVRInteractable>().IsAttached)
+    //                mRenderer.enabled = true;
+    //        }
+    //    }
+    //}
 
-            attached = null;
-        }
-    }
+    //public void AttachObject(GameObject o)
+    //{
+    //    if (!attached)
+    //    {
+    //        attached = o;
+    //        attached.transform.position = transform.position;
+    //        attached.transform.up = transform.up;
+
+    //        if (attached.GetComponent<ToolScript>())
+    //            attached.GetComponent<ToolScript>().SetReady(true);
+    //    }
+    //}
+
+    //public void DetachObject()
+    //{
+    //    if (attached)
+    //    {
+    //        if (attached.GetComponent<ToolScript>())
+    //            attached.GetComponent<ToolScript>().SetReady(false);
+
+    //        attached = null;
+    //    }
+    //}
 }
--- Assets/EXVR-Forge/Scripts/Tools/BendingToolScript.cs
+++ Assets/EXVR-Forge/Scripts/Tools/BendingToolScript.cs
@@ -4,46 +4,46 @@ using System.Collections.Generic;
 
 public class BendingToolScript : ToolScript
 {
-    public Object bendPrefab;
+    //public Object bendPrefab;
 
-    private GameObject bendInstance;
+    //private GameObject bendInstance;
 
-    public override void Start()
-    {
-        base.Start();
-    }
+    //public override void Start()
+    //{
+    //    base.Start();
+    //}
     
-    protected override void OnAttach()
-    {
-        if (bendPrefab)
-        {
-            bendInstance = Instantiate(bendPrefab) as GameObject;
-            bendInstance.transform.parent = attached.transform;
-            bendInstance.transform.position = attachPoint.transform.position;
-            bendInstance.transform.localScale = Vector3.one;
+    //protected override void OnAttach()
+    //{
+    //    if (bendPrefab)
+    //    {
+    //        bendInstance = Instantiate(bendPrefab) as GameObject;
+    //        bendInstance.transform.parent = attached.transform;
+    //        bendInstance.transform.position = attachPoint.transform.position;
+    //        bendInstance.transform.localScale = Vector3.one;
 
-            Bend bendScript = bendInstance.GetComponent<Bend>();
+    //        Bend bendScript = bendInstance.GetComponent<Bend>();
 
-            Transform[] attachedTransforms = attached.transform.GetComponentsInChildren<Transform>();
-            List<GameObject> attachedChildren = new List<GameObject>();
-            for (int i = 0; i < attachedTransforms.Length; i++)
-            {
-                if (attachedTransforms[i].GetComponent<MeshFilter>())
-                    attachedChildren.Add(attachedTransforms[i].gameObject);
-            }
+    //        Transform[] attachedTransforms = attached.transform.GetComponentsInChildren<Transform>();
+    //        List<GameObject> attachedChildren = new List<GameObject>();
+    //        for (int i = 0; i < attachedTransforms.Length; i++)
+    //        {
+    //            if (attachedTransforms[i].GetComponent<MeshFilter>())
+    //                attachedChildren.Add(attachedTransforms[i].gameObject);
+    //        }
 
-            bendScript.SetMeshes(attachedChildren.ToArray());
-            bendInstance.transform.LookAt(attached.transform.position);
-            bendInstance.transform.Rotate(new Vector3(-90f, 0f, 0f));
-        }
-    }
+    //        bendScript.SetMeshes(attachedChildren.ToArray());
+    //        bendInstance.transform.LookAt(attached.transform.position);
+    //        bendInstance.transform.Rotate(new Vector3(-90f, 0f, 0f));
+    //    }
+    //}
 
-    protected override void OnDetach()
-    {
-        if (bendInstance)
-        {
-            Destroy(bendInstance);
-            bendInstance = null;
-        }
-    }
+    //protected override void OnDetach()
+    //{
+    //    if (bendInstance)
+    //    {
+    //        Destroy(bendInstance);
+    //        bendInstance = null;
+    //    }
+    //}
 }
