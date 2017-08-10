da1a67e786584fa9a5df24e9a77d8da531d848ed
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-08-10 23:32:13 +0100

    Cleaned up old script started bending and filling room

---
+++ Assets/EXVR-Forge/Scripts/Tools/BendTool.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BendTool : MonoBehaviour {
+
+	//re use old code to hold rod in place when in bending rod
+    //port old bending code
+
+}
