b298f32ab5da416cb4a0399140350d79950b3f13
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-08-11 11:11:12 +0100

    New mesh deforming functionality.

--- Assets/EXVR-Forge/Scripts/DeformableMesh.cs
+++ Assets/EXVR-Forge/Scripts/DeformableMesh.cs
@@ -17,26 +17,66 @@ public class DeformableMesh : MonoBehaviour
         mCollider = GetComponent<MeshCollider>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionExit(Collision collision)
     {
-        Deform(other.ClosestPointOnBounds(transform.position));
+        Vector3 centralContact = Vector3.zero;
+        for (int i = 0; i < collision.contacts.Length; i++)
+            centralContact += collision.contacts[i].point;
+        centralContact /= collision.contacts.Length;
+
+        Deform(collision.transform, centralContact);
     }
 
-    private void Deform(Vector3 hitPoint)
+    private void OnTriggerExit(Collider other)
     {
+        RaycastHit hitInfo;
+        float rayLength = other.bounds.size.magnitude * 1.1f;
+
+        if (Physics.Raycast(other.transform.position, other.transform.forward, out hitInfo, rayLength) 
+            || Physics.Raycast(other.transform.position, -other.transform.forward, out hitInfo, rayLength))
+        {
+            if (hitInfo.transform.gameObject == gameObject)
+            {
+                Deform(other.transform, hitInfo.point);
+                return;
+            }
+        }
+
+        Deform(other.transform, other.transform.position);
+    }
+
+    float startTime = 0.0f;
+    private void Deform(Transform otherObject, Vector3 hitPoint)
+    {
+        startTime = Time.realtimeSinceStartup;
+
+        Vector3 impactVectorNormalized = (otherObject.position - hitPoint).normalized;
+        Debug.DrawLine(otherObject.position, hitPoint, Color.red, Mathf.Infinity);
+        Vector3 simplifiedHitPoint = DivideVector3(hitPoint - transform.position, transform.lossyScale);
         Vector3[] mVertices = mFilter.sharedMesh.vertices;
 
         for (int i = 0; i < mVertices.Length; i++)
         {
-            Vector3 diff = transform.position + mVertices[i] - hitPoint;
+            Vector3 diff = simplifiedHitPoint - mVertices[i];
             float dist = diff.magnitude;
             float influence = Mathf.Clamp01(maxInfluence - dist);
 
-            mVertices[i] += (diff * (1.0f / dist) * forceFactor) * influence;
+            // This line helps with sideways displacment but needs some work.
+            //Vector3 displacement = diff * (1.0f / dist) * forceFactor * influence;
+
+            Vector3 displacement = -impactVectorNormalized * forceFactor * influence;
+            mVertices[i] += displacement;
         }
 
         mFilter.sharedMesh.vertices = mVertices;
         mFilter.sharedMesh.RecalculateBounds();
         mCollider.sharedMesh = mFilter.sharedMesh;
+
+        Debug.Log(Time.realtimeSinceStartup - startTime);
+    }
+
+    private Vector3 DivideVector3(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
     }
 }
