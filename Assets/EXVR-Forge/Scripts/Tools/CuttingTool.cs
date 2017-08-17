4de97585d64054699c163d33a30909388a326ff1
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-08-17 11:45:22 +0100

    Added Cutting tool ability to hold Rod

---
+++ Assets/EXVR-Forge/Scripts/Tools/CuttingTool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class CuttingTool : MonoBehaviour {
+
+    public string tooltag;
+
+    private Rigidbody attachedRb;
+    private GameObject parent;
+
+    void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+        parent = this.transform.parent.gameObject;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == tooltag)
+        {
+            parent.GetComponent<BoxCollider>().enabled = false;
+
+            if (!other.GetComponent<Throwable>().attached)
+            {
+                GetComponent<Collider>().enabled = false;
+                attachedRb = other.GetComponent<Rigidbody>();
+                attachedRb.constraints = RigidbodyConstraints.FreezeAll;
+            }
+            else
+            {
+                other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
+
+    public void ReEnableCutTool()
+    {
+        parent.GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Collider>().enabled = true;
+    }
+
+    //on collide and side button press
+
+    //spawn bend prefab
+
+
+}
