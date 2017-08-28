040102caa0039fdf7aedcd0293aa032fa614bcc7
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-08-28 13:07:46 +0100

    Added sounds for metal and wood

---
+++ Assets/EXVR-Forge/Scripts/WaterScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterScript : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        //if (other.tag == "Maluable" && other.GetComponent<PerVertexScript>())
+        //    other.GetComponent<PerVertexScript>().Heat(-0.05f);
+    }
+}
