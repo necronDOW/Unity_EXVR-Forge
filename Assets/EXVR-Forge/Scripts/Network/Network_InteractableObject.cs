e6ecb2fcaf1c50efd191e9d7b68ad8ee60a8ed14
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-08-31 14:19:55 +0100

    Added networked attach variables.

--- Assets/EXVR-Forge/Scripts/Network/Network_InteractableObject.cs
+++ Assets/EXVR-Forge/Scripts/Network/Network_InteractableObject.cs
@@ -7,9 +7,12 @@ using Valve.VR.InteractionSystem;
 [RequireComponent(typeof(InteractableHoverEvents))]
 public class Network_InteractableObject : NetworkBehaviour
 {
+    public bool isAttached = false;
+
     private void Start()
     {
         GetComponent<InteractableHoverEvents>().onAttachedToHand.AddListener(OnGrab);
+        GetComponent<InteractableHoverEvents>().onDetachedFromHand.AddListener(OnRelease);
     }
 
     public void OnGrab()
@@ -17,5 +20,23 @@ public class Network_InteractableObject : NetworkBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("VRLocalPlayer");
         NetworkIdentity playerID = player.GetComponent<NetworkIdentity>();
         player.GetComponent<Network_PlayerController>().CmdOnGrab(GetComponent<NetworkIdentity>().netId, playerID);
+        CmdOnPickup();
+    }
+
+    public void OnRelease()
+    {
+        CmdOnRelease();
+    }
+
+    [Command]
+    public void CmdOnPickup()
+    {
+        isAttached = true;
+    }
+
+    [Command]
+    public void CmdOnRelease()
+    {
+        isAttached = false;
     }
 }
