d6d4ddcfeaa2b48b0dafb14e160f0ae874b5a43f
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-10-31 19:09:06 +0000

    Cleaning up scripts for tools

--- Assets/EXVR-Forge/Scripts/Tools/BendTool.cs
+++ Assets/EXVR-Forge/Scripts/Tools/BendTool.cs
@@ -30,31 +30,6 @@ public class BendTool : AnvilTool
     {
         gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
     }
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if (other.tag == tooltag)
-    //    {
-    //        parent.GetComponent<BoxCollider>().enabled = false;
-
-    //        if (!other.GetComponent<Throwable>().attached)
-    //        {
-    //            GetComponent<Collider>().enabled = false;
-    //            attachedRb = other.GetComponent<Rigidbody>();
-    //            attachedRb.constraints = RigidbodyConstraints.FreezeAll;
-    //        }
-    //        else
-    //        {
-    //            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-    //        }
-    //    }
-    //}
-
-    //public void ReEnableBendTool()
-    //{
-    //    parent.GetComponent<BoxCollider>().enabled = true;
-    //    GetComponent<Collider>().enabled = true;
-    //}
-
     //on collide and side button press
 
     //spawn bend prefab
