29fa27a34eb582067d47616996c525ad0ef3ea4e
Liam Wilson <liamryanwilson@hotmail.co.uk>
2018-01-13 19:42:10 +0000

    Fixed Look At Script, YAY!

--- Assets/EXVR-Forge/Scripts/LookAtScript.cs
+++ Assets/EXVR-Forge/Scripts/LookAtScript.cs
@@ -4,46 +4,30 @@ using UnityEngine;
 
 public class LookAtScript : MonoBehaviour
 {
-    public enum ForwardAxis
-    {
-        X,
-        Y,
-        Z
-    }
 
     public static Vector3 nullTarget = new Vector3(int.MinValue, int.MinValue, int.MinValue);
+    public Transform targetTransform;
     public Vector3 target = nullTarget;
-    public bool xAxis = true;
-    public bool yAxis = true;
-    public bool zAxis = true;
-    public ForwardAxis forwardAxis = ForwardAxis.Z;
+
+    private Quaternion originalRotation;
+
+    private void Start()
+    {
+        originalRotation = transform.rotation;
+    }
 
     private void Update()
     {
+        if (targetTransform)
+            target = targetTransform.position;
+
         if (target != nullTarget) {
-            Vector3 relativePosition = target - transform.position;
-            relativePosition = new Vector3(
-                xAxis ? 0 : relativePosition.x,
-                yAxis ? 0 : relativePosition.y,
-                zAxis ? 0 : relativePosition.z);
+            Vector3 targetPoint = new Vector3(transform.position.x, target.y, target.z) - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.right);
 
-            Quaternion allAxesLookAt = Quaternion.LookRotation(relativePosition, transform.up);
-            TranslateRelativeToForwardAxis(ref allAxesLookAt);
-            transform.eulerAngles = allAxesLookAt.eulerAngles;
-        }
-    }
-
-    private void TranslateRelativeToForwardAxis(ref Quaternion original)
-    {
-        switch (forwardAxis) {
-            case ForwardAxis.X:
-                original *= Quaternion.Euler(0.0f, 90.0f, 0.0f);
-                break;
-            case ForwardAxis.Y:
-                original *= Quaternion.Euler(90.0f, 0.0f, 0.0f);
-                break;
-            default: case ForwardAxis.Z:
-                break;
+            transform.rotation = originalRotation;
+            transform.rotation *= Quaternion.Euler(0, 0, 90) * Quaternion.Euler(90, 0, 0);
+            transform.rotation *= targetRotation;
         }
     }
 }
